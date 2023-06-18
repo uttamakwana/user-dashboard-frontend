ac8fd69d82fbd20023af9a7e9f668de623038c28
Uttam Makwana <uttamakwana4503@gmail.com>
2023-06-18 13:12:13 +0530

    Not responsive but working sidebar height

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,19 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./sidebar.css";
 
 const Sidebar = () => {
+  const [headerHeight, setHeaderHeight] = useState(
+    document.querySelector(".header")
+      ? document.querySelector(".header").offsetHeight
+      : 0
+  );
+  const [sidebarHeight, setSidebarHeight] = useState(0);
+  useEffect(() => {
+    const handleSidebarHeight = () => {
+      setHeaderHeight(document.querySelector(".header").offsetHeight);
+      console.log("Screen size", window.innerHeight);
+      console.log("Header size", headerHeight);
+      let screenHeight = window.innerHeight;
+      setSidebarHeight(screenHeight - headerHeight);
+    };
+
+    handleSidebarHeight();
+
+    window.addEventListener("resize", handleSidebarHeight);
+
+    return () => {
+      window.removeEventListener("resize", handleSidebarHeight);
+    };
+  }, []);
+
+  console.log(sidebarHeight);
+  console.log(headerHeight);
   return (
-    <aside className="sidebar">
-      <div className="sidebar__container">
-        <ul className="sidebar__container-list">
+    <aside
+      className="sidebar flex-center"
+      style={{
+        height: sidebarHeight,
+        top: headerHeight,
+      }}
+    >
+      <div className="sidebar__container flex-center">
+        <ul className="sidebar__container__list">
           <li className="sidebar__container__list__item">
-            <a href="#home" className="sidebar__container__list__link__item">
-              Profile
+            <a href="#home" className="sidebar__container__list__item__link">
+              👤Profile
             </a>
           </li>
           <li className="sidebar__container__list__item">
-            <a href="#home" className="sidebar__container__list__link__item">
-              Contact
+            <a href="#home" className="sidebar__container__list__item__link">
+              📃Contact
+            </a>
+          </li>
+          <li className="sidebar__container__list__item">
+            <a href="#home" className="sidebar__container__list__item__link">
+              🛞 Settings
             </a>
           </li>
         </ul>
--- src/context/ContextProvider.jsx
+++ src/context/ContextProvider.jsx
@@ -1,13 +1,8 @@
 import React, { createContext } from "react";
-import HeaderStateProvider from "./HeaderStateProvider";
 
 export const Context = createContext();
 const ContextProvider = ({ children }) => {
-  return (
-    <Context.Provider value={{}}>
-      <HeaderStateProvider>{children}</HeaderStateProvider>
-    </Context.Provider>
-  );
+  return <Context.Provider value={{}}>{children}</Context.Provider>;
 };
 
 export default ContextProvider;
