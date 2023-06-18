eb63759a6327aa348e69afdb0f575543f47a23fa
Uttam Makwana <uttamakwana4503@gmail.com>
2023-06-18 09:55:52 +0530

    Added Logo, Profile ,and Logout images in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,41 @@
-import "./header.css"
+import "./header.css";
+import images from "../../constants/images";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="header">
+      <nav className="header__navbar">
+        <div className="header__navbar__logo-container">
+          <img
+            src={images.Logo}
+            alt="Logo"
+            id="header__navbar__logo-container__logo"
+          />
+          <span>Dashboard</span>
+        </div>
+        <div className="header__navbar__list-container">
+          <ul className="header__navbar__list-container__list">
+            <li className="header__navbar__list-container-list-item">
+              <a
+                href="#home"
+                className="header__navbar__list-container__list__item__link"
+              >
+                <img src={images.Profile} alt="Profile img" />
+              </a>
+            </li>
+            <li className="header__navbar__list-container-list-item">
+              <a
+                href="#home"
+                className="header__navbar__list-container__list__item__link"
+              >
+                <img src={images.LogOut} alt="Profile img" />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
---
+++ src/constants/images.js
@@ -0,0 +1,9 @@
+import Profile from "../assets/images/profile.png";
+import LogOut from "../assets/images/logout.png";
+import Logo from "../assets/images/logo.png";
+
+export default {
+  Profile,
+  LogOut,
+  Logo
+};
